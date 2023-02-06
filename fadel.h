e1a869ddc18dac832579a35905884cf96ebb7083
famolah1717 <famolah17@gmail.com>
2023-02-06 10:52:15 +0700

    push fadel.c fadel.h

---
+++ fadel.h
@@ -0,0 +1,9 @@
+#ifndef trigonometri_h
+#define trigonometri_h
+#include <math.h>
+
+double HitungSin(double rad);
+double HitungCos(double rad);
+double HitungTan(double rad);
+void MenuTrigonometri();
+#endif
