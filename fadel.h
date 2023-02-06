fd9350b8524ac5df3ccefa3b8bdf7e45b0e9573a
famolah1717 <famolah17@gmail.com>
2023-02-06 11:00:53 +0700

    removing fadel.c fadel.h

--- fadel.h
+++
@@ -1,9 +0,0 @@
-#ifndef trigonometri_h
-#define trigonometri_h
-#include <math.h>
-
-double HitungSin(double rad);
-double HitungCos(double rad);
-double HitungTan(double rad);
-void MenuTrigonometri();
-#endif
