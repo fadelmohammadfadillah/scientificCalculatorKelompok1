0a58be71582aa1930dcba75b68121eba715a5f08
aruya <116321705+aruya2707@users.noreply.github.com>
2023-02-12 14:41:50 +0700

    Update alya1.h
    up median and mode

--- alya1.h
+++ alya1.h
@@ -4,72 +4,113 @@
 #define alya1.H
 #ifndef alya1.H
 
-float ProgramStatistika(int n);
-void sort(int num[100], int n);
-void swap(int *a, int *b);
+
+void ProgramStatistika();
+float Mean(float num[], int n);
+float Median(float num[], int n);
+float Modus(float num[], int n);
 
 #endif
 
 
-
-float ProgramStatistika(){
-	int i;
-	int n=5;
-	int d;
-	float sum=0.0, mean;
+void ProgramStatistika(){
+	int i,d,n;
 	float num[100];
-printf("pilihan anda :\n");
-printf("1.Mean\n 2.Median\n ");
-scanf("%d",&d);
-switch(d){
-	//mean
-	case 1:
-
+	
+	printf("Masukkan Jumlah Data :\n");
+	scanf("\n\n %d", &n);
+	
+	printf("Masukkan Data Anda (dipisahkan oleh enter) :\n");
 	for(i=0; i<n; i++){
-		printf("%d. Masukan : ", i+1);
 		scanf("%f", &num[i]);
-		sum += num[i];	
 	}
-	mean = sum / n;
-	printf("mean = %.2f", mean);
-	system ("\npause");
-	break;
 	
+	printf("Masukkan Pilihan Anda :");
+	printf("\n\t1.Mean\n\t 2.Median\n\t 3.Modus\n");
+	scanf("\n\n %d", &d);
+	getchar();
+	switch(d){
+		case 1: //mean
+			Mean(num, n);
+			break;
+		case 2: //Median
+			Median(num, n);
+			break;
+		case 3:
+			Modus(num, n);
+			break;
+	}
+	}
 	
-	//median
-//	case 2:
-//	for(i=0; i<n; i++){
-//	printf("%d. Masukan : ", i+1);
-//	scanf("%f", &num[i]);
-//	}
-//	sort(num, n);
-//	int tengah = n/2;
-//	if(n%2==0){
-//		printf("median : %.2f\n",(float)(num[tengah-1]+ num[tengah])/2);
-//	}else {
-//		printf("Median : %d\n", num[tengah]);
-//	}
-//	system("\npause");
-//	break;
+
+
+float Mean(float num[], int n){
+	int i;
+	float sum=0.0, mean;
 	
+	for(i=0; i<n; i++){
+		sum += num[i];
+	}
+	mean = sum/n;
+	
+	printf("Mean dari data tersebut adalah: %.2f\n", mean);
+	return mean;
 }
+
+float Median(float num[], int n){
+	int i,j;
+	float median;
+	
+	for(i=0; i<n; i++){
+		for(j=0; j<n-i-1; j++){
+			if(num[j] > num[j+1]){
+				float temp = num[j];
+				num[j] = num[j+1];
+				num[j+1] = temp;
+			}
+		}
+	}
+	
+	if(n%2 == 0){
+		median = (num[(n-1)/2] + num[n/2])/2.0;
+	} else {
+		median = num[n/2];
+	}
+	
+	printf("Median dari data tersebut adalah: %.2f\n", median);
+	
+	return median;
+	}
+
+
+float Modus(float num[], int n){
+  int i, j, max=0, index=0;
+  float modus;
+  int freq[100];
+
+  for(i=0; i<n; i++){
+    freq[i] = 1;
+    for(j=0; j<i; j++){
+      if(num[i] == num[j]){
+        freq[j]++;
+        break;
+      }
+    }
+  }
+
+  for(i=0; i<n; i++){
+    if(freq[i] > max){
+      max = freq[i];
+      index = i;
+    }
+  }
+
+  modus = num[index];
+
+  if(max == 1)
+    printf("Data tidak memiliki modus\n");
+  else
+    printf("Modus dari data tersebut adalah: %.2f\n", modus);
+
+  return modus;
 }
-
-
-//void sort(int num[100], int n){
-//	int j,k;
-//	 for (int j = 0; j < n-1; j++) {
-//        for (int k = 0; k < n-j-1; k++) {
-//            if (num[k] > num[k+1]) {
-//                swap(&num[k], &num[k+1]);
-//            }
-//        }
-//    }
-//}
-
-//
-//void swap(int *a, int *b) {
-//    int temp = *a;
-//    *a = *b;
-//    *b = temp;
-//}
