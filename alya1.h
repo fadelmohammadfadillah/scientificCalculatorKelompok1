84ba40a3bd6e9cb6890cdfe6841bba3defce2a6d
aruya <116321705+aruya2707@users.noreply.github.com>
2023-05-20 23:39:23 +0700

    nambahin kondisi

--- alya1.h
+++ alya1.h
@@ -4,24 +4,24 @@
 #include <conio.h>
 #define PI 3.14159
 
-//program untuk invers trigonometri
+//Program yang membantu perhitungan invers
+double sinus(double x) { // x dalam bentuk radian
+    double hasil = x;
+    double pangkat = x;    // Menyimpan nilai pangkat dari x dalam deret taylor
+    double faktorial = 1;  // Menyimpan nilai faktorial pada perhitungan deret taylor
+    int i;
+    int tanda = -1;        // Digunakan untuk mengubah tanda dari setiap suku deret
 
-double sinus(double x) { //berupa sudut dalam bentuk radian
-    double hasil = x;	//akan menyimpan hasil perhitungan sinus nantinya
-    double pangkat = x;		//menyimpan nilai pangkat dari x dalam perhitungan deret tylor
-    double faktorial = 1;	//menyimpan nilai faktorial pada perhitungan deret tylor
-    int i;	/*merepresentasikan bilangan ganjil dalam iterasi, untuk menghitung 
-	faktorial dari bilangan tersebut dan bilangan sebelumnya*/
-    int tanda = -1;		//digunakan untuk mengubah tanda dari setiap suku deret
-    for ( i = 3; i <= 19; i += 2) {
+    // Pendekatan menggunakan perhitungan deret taylor
+    for (i = 3; i <= 19; i += 2) {
         pangkat *= x * x;
         faktorial *= i * (i - 1);
         hasil += tanda * (pangkat / faktorial);
-        tanda *= -1;	//nilai akan bergantian positif dan negatif saat iterasi agar hasil lebih akurat
+        tanda *= -1;        // Nilai akan bergantian positif dan negatif saat iterasi agar hasil lebih akurat
     }
+
     return hasil;
 }
-
 double kosinus(double x) {
     double hasil = 1;
     double pangkat = 1;
@@ -38,43 +38,60 @@ double kosinus(double x) {
     }
     return hasil;
 }
-
 double tangen(double x) { // fungsi tangen dalam radian
-    return sinus(x) / kosinus(x);
+		return sinus(x) / kosinus(x);		
 }
-
-//double tangen(double x) {
-//    double hasil = x; //menyimpan hasil perhitungan tangen nantinya
-//    double pangkat = x; //menyimpan nilai pangkat dari x dalam perhitungan deret tylor
-//    double faktorial = 1; //menyimpan nilai faktorial pada perhitungan deret tylor
-//    double tambahan = x; //nilai tambahan dari setiap suku deret
-//    int i; /*merepresentasikan bilangan ganjil dalam iterasi, untuk menghitung 
-//        faktorial dari bilangan tersebut dan bilangan sebelumnya*/
-//    int tanda = -1; //digunakan untuk mengubah tanda dari setiap suku deret
-//    for (i = 3; i <= 19; i += 2) {
-//        pangkat *= x * x;
-//        faktorial *= i * (i - 1);
-//        tambahan = tanda * (pangkat / faktorial);
-//        hasil += tambahan;
-//        tanda *= -1; //nilai akan bergantian positif dan negatif saat iterasi agar hasil lebih akurat
-//    }
-//    return hasil;
-//}
-
-double HitungCosec(double derajat) { //fungsi cosec dalam derajat
+double HitungCosec(double derajat) {
     double rad = derajat * PI / 180;
-    return 1 / sinus(rad);
+    double sin_val = sinus(rad);
+
+    // Memeriksa kondisi tak terdefinisi
+    if (sin_val == 0) {
+        printf("Nilai tak terdefinisi (undefined) untuk cosec(%lf)\n", derajat);
+        return 0.0;
+    } else {
+        if (derajat == 180 || derajat == 360){
+        	printf("Nilai tak terdefenisi (undefined) untuk cosec(%lf)\n", derajat);
+		}else{
+			return 1 / sin_val;
+		}
+    }
+    
+}
+double HitungSec(double derajat) {
+    double rad = derajat * PI / 180;
+    double cos_val = kosinus(rad);
+
+    // Memeriksa kondisi tak terdefinisi
+    if (cos_val == 0) {
+        printf("Nilai tak terdefinisi (undefined) untuk sec(%lf)\n", derajat);
+        return 0.0;
+    } else {
+    	if (derajat == 90 || derajat == 270){
+        	printf("Nilai tak terdefenisi (undefined) untuk secan(%lf)\n", derajat);
+		}else{
+			return 1 / cos_val;
+		}
+    }
+}
+double HitungCotan(double derajat) {
+    double rad = derajat * PI / 180;
+    double tan_val = tangen(rad);
+
+    // Memeriksa kondisi tak terdefinisi
+    if (tan_val == 0) {
+        printf("Nilai tak terdefinisi (undefined) untuk cotan(%lf)\n", derajat);
+        return 0.0;
+    } else {
+    	if (derajat == 180 || derajat == 360){
+        	printf("Nilai tak terdefenisi (undefined) untuk cosec(%lf)\n", derajat);
+		}else{
+			return 1 / tan_val;
+		}
+    }
 }
 
-double HitungSec(double derajat) { //fungsi sec dalam derajat
-    double rad = derajat * PI / 180;
-    return 1 / kosinus(rad);
-}
-
-double HitungCotan(double derajat) { //fungsi cot dalam derajat
-    double rad = derajat * PI / 180;
-    return 1 / tangen(rad);
-}
+//program untuk invers trigonometri
 
 double Inverse_Sin(double angka) {
     double batas_bawah = -1.0;
@@ -83,6 +100,7 @@ double Inverse_Sin(double angka) {
     double tebakan = (batas_atas + batas_bawah) / 2.0;
     double nilai = sinus(tebakan);
 
+	//penggunaan binary search dimana dibagi dua kategori
     while ((nilai - angka) > toleransi || (angka - nilai) > toleransi) {
         if (nilai > angka) {
             batas_atas = tebakan;
@@ -106,6 +124,7 @@ double Inverse_Cos(double angka) {
     int max_iterasi = 100;
     int iterasi = 0;
 
+	//penggunaan binary search dimana dibagi dua kategori
     while ((nilai - angka) > toleransi || (angka - nilai) > toleransi) {
         if (nilai > angka) {
             batas_atas = tebakan;
@@ -121,13 +140,13 @@ double Inverse_Cos(double angka) {
 }
 
 double Inverse_Tan(double angka){
-	//penggunaan binary search dimana dibagi dua kategori
     double batas_atas = PI / 2.0;
     double batas_bawah = -PI / 2.0;
     double inverse = (batas_atas + batas_bawah) / 2.0;
     int iterations = 10;	//10 kali iterasi pencarian nilai 
     int i;
 
+	//penggunaan binary search dimana dibagi dua kategori
     for (i = 0; i < iterations; i++) {
         double tan_val = tan(inverse * PI / 180.0);
         /*jika nilai tangent lebih besar dr angka yg ingin dicari maka batas atas diganti*/
