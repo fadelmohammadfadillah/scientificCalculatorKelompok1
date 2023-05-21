6994fa27aab03d56109dccbb6c8094ef31e296ff
RenoSebastian_290404 <116626332+BobbyBob669@users.noreply.github.com>
2023-05-21 16:49:32 +0700

    Update reno.h

--- reno.h
+++ reno.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "adi.h"
 
-double logNature(double angka) // modul log natural, fungsi ini menerima sebuah argumen berupa bilangan yang akan dihitung logaritma basis e nya, dengan parameter angka berjenis input
-	{
-     if (angka <= 0) {
-        return NAN; // NaN (Not a Number) jika angka negatif atau nol
+double LogNature(double angka) // modul log natural, fungsi ini menerima sebuah argumen berupa bilangan yang akan dihitung logaritma basis e nya, dengan parameter angka berjenis input
+	{ 
+	 if (angka <= 0) {
+     	printf("[ERROR]: Argumen harus lebih dari 0");
+        return -1; // NaN (Not a Number) jika angka negatif atau nol
 	}
 	 //deklarasi variabel hasil dan term yang akan digunakan sebagai penampung.
      double hasil = 0.0;
@@ -12,7 +13,6 @@ double logNature(double angka) // modul log natural, fungsi ini menerima sebuah 
      double temp = (angka - 1) / angka; 
 	 //term menyimpan nilai suku deret taylor
      double i = 1;
-     
      if(temp < 0)//untuk mengganti FABS yang ada pada liblary math.h agar hasil tetap positif
 	{
     	temp = -(temp); //agar nilai yang negatif diubah menjadi positif
@@ -20,7 +20,7 @@ double logNature(double angka) // modul log natural, fungsi ini menerima sebuah 
      while (temp > 0.0000000001) { // hingga suku terakhir sudah sangat kecil
         hasil = hasil + temp;
         i++;
-        temp = pangkat((angka - 1) / angka, i) / i; //menggunakan fungsi pangkat yang ada pada adi.h
+        temp = pangkat ((angka - 1) / angka, i) / i; //menggunakan fungsi pangkat yang ada pada adi.h
     }
      return hasil;
 }
