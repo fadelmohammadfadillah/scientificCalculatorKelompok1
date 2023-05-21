21b05b32ecee6b835970b1ddb4516b27ee235e3d
RenoSebastian_290404 <116626332+BobbyBob669@users.noreply.github.com>
2023-05-22 05:07:39 +0700

    Update comment

--- reno.h
+++ reno.h
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "adi.h"
 
-double logNature(double angka) // modul log natural, fungsi ini menerima sebuah argumen berupa bilangan yang akan dihitung logaritma basis e nya, dengan parameter angka berjenis input
-	{ 
+double logNature(double angka) {
+	/* modul log natural, fungsi ini menerima sebuah argumen berupa bilangan yang akan dihitung logaritma basis e nya, dengan parameter angka berjenis input 
+	 Rumus deret Taylor untuk logaritma natural (ln) adalah:
+	 ln(x) = (x - 1) - (x - 1)^2/2 + (x - 1)^3/3 - (x - 1)^4/4 + ... (dan seterusnya hingga kriteria toleransi atau batasan akurasi yang diinginkan. semakin jauh nilai x dari 1 maka semakin banyak perulangan yang dilakukan.
+	 saya mengambil batasan toleransi hingga 1e^-10)
+	 agar memenuhi tujuan tidak menggunakan math.h modul ini juga menggunakan implementasi modul pangkat yang berasal dari adi.h*/ 
 	 if (angka <= 0) {
      	printf("[ERROR]: Argumen harus lebih dari 0");
         return -1; // NaN (Not a Number) jika angka negatif atau nol
@@ -11,7 +15,7 @@ double logNature(double angka) // modul log natural, fungsi ini menerima sebuah 
      double hasil = 0.0;
 	 //hasil digunakan untuk menyimpan hasil perhitungan log berbasis e
      double temp = (angka - 1) / angka; 
-	 //term menyimpan nilai suku deret taylor
+	 //temp menyimpan nilai suku deret taylor
      double i = 1;
      if(temp < 0)//untuk mengganti FABS yang ada pada liblary math.h agar hasil tetap positif
 	{
