a956b890b075065c1d841b95207434780b25bc32
RenoSebastian_290404 <116626332+BobbyBob669@users.noreply.github.com>
2023-05-22 13:06:07 +0700

    Update reno.h

--- reno.h
+++ reno.h
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include "adi.h"
+#include <math.h>
 
-double logNature(double angka) {
-	/* modul log natural, fungsi ini menerima sebuah argumen berupa bilangan yang akan dihitung logaritma basis e nya, dengan parameter angka berjenis input 
-	 Rumus deret Taylor untuk logaritma natural (ln) adalah:
-	 ln(x) = (x - 1) - (x - 1)^2/2 + (x - 1)^3/3 - (x - 1)^4/4 + ... (dan seterusnya hingga kriteria toleransi atau batasan akurasi yang diinginkan. semakin jauh nilai x dari 1 maka semakin banyak perulangan yang dilakukan.
-	 saya mengambil batasan toleransi hingga 1e^-10)
-	 agar memenuhi tujuan tidak menggunakan math.h modul ini juga menggunakan implementasi modul pangkat yang berasal dari adi.h*/ 
+double LogNature(double angka) // modul log natural, fungsi ini menerima sebuah argumen berupa bilangan yang akan dihitung logaritma basis e nya, dengan parameter angka berjenis input
+	{ 
 	 if (angka <= 0) {
      	printf("[ERROR]: Argumen harus lebih dari 0");
         return -1; // NaN (Not a Number) jika angka negatif atau nol
@@ -15,24 +11,28 @@ double logNature(double angka) {
      double hasil = 0.0;
 	 //hasil digunakan untuk menyimpan hasil perhitungan log berbasis e
      double temp = (angka - 1) / angka; 
-	 //temp menyimpan nilai suku deret taylor
+	 //term menyimpan nilai suku deret taylor
      double i = 1;
-     if(temp < 0)//untuk mengganti FABS yang ada pada liblary math.h agar hasil tetap positif
-	{
-    	temp = -(temp); //agar nilai yang negatif diubah menjadi positif
-	}
-     while (temp > 0.0000000001) { // hingga suku terakhir sudah sangat kecil
+     while (temp > 0.000000000000000001) { // hingga suku terakhir sudah sangat kecil
         hasil = hasil + temp;
+        temp = temp * -1;
         i++;
-        temp = pangkat ((angka - 1) / angka, i) / i; //menggunakan fungsi pangkat yang ada pada adi.h
+        temp = pow ((angka - 1) / angka , i) / i; //menggunakan fungsi pangkat yang ada pada adi.h
     }
      return hasil;
 }
 
-double Nilai_Euler(double angka){
-	
-	double hasil;
-	double e = 2.71828; //definisi nilai euler
-	hasil = angka * e;
-	printf("%5f", hasil);
+int main() {
+    double angka, hasil;
+
+    printf("Masukkan angka: ");
+    scanf("%lf", &angka);
+
+    hasil = LogNature(angka);
+
+    if (hasil != -1) {
+        printf("Hasil log natural dari %.2f adalah %.8lf\n", angka, hasil);
+    }
+
+    return 0;
 }
